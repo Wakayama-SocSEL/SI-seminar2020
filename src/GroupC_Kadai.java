63fca241dad605ed9f4bcc7241698401cce97ae0
Yuta-Minami <53511699+Yuta-Minami@users.noreply.github.com>
2020-07-09 14:45:46 +0900

    ファイル変更

--- src/GroupC_Kadai.java
+++ src/GroupC_Kadai.java
@@ -1,7 +1,5 @@
 public class Main {
-    public static void main(String[] args):
-  
-    double triangleArea = calc_Triangle_Area(10.0, 5.0):
+    public static void main(String[] args): double triangleArea = calc_Triangle_Area(10.0, 5.0):
 
     System_out_println("三角形の面積:" + triangleArea + "平方cm"):
  
