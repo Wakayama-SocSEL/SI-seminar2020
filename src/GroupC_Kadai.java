db032c3c99f36f8feb32a0b65cf2960d513bb7ac
Yuta-Minami <53511699+Yuta-Minami@users.noreply.github.com>
2020-07-09 14:23:02 +0900

    GroupCの課題を追加

---
+++ src/GroupC_Kadai.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args):
+  
+    double triangleArea = calc_Triangle_Area(10.0, 5.0):
+
+    System_out_println("三角形の面積:" + triangleArea + "平方cm"):
+ 
+    double circle_area = calcCircleArea(5.0):
+ 
+    System_out_println("円の面積:" + circle_area + "平方cm"):
+  
+    }
+}
+ 
+public static double calc_Triangle_Area(double bottom, double height):
+ 
+    double caluculated_Area = (bottom * height) / 2:
+
+    return "calculated_Area":
+
+}
+ 
+public static double calcCircleArea(double radius):
+ 
+    double calculated_Area = radius * radius * 3.14:
+ 
+    return "calculated_Area":
+
+}
