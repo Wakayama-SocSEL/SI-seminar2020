ed415ecad5ad6d51c73e311d52334d2dddf25fd7
Ryota Ando <andoooo9.y.1127@gmail.com>
2020-07-15 17:49:26 +0900

    Update GroupB_Kadai.java

--- src/GroupB_Kadai.java
+++ src/GroupB_Kadai.java
@@ -1,101 +1,101 @@
-index,anime_id,name,genre,type,episodes,rating,members
-0,14741,Chuunibyou demo Koi ga Shitai!,Comedy,Drama,Romance,School,Slice of Life,TV,12,7.95,394399
-1,1943,Paprika,Fantasy,Horror,Mystery,Psychological,Sci-Fi,Thriller,Movie,1,8.15,163308
-2,22535,Kiseijuu: Sei no Kakuritsu,Action,Drama,Horror,Psychological,Sci-Fi,Seinen,TV,24,8.59,425457
-3,43,Ghost in the Shell,Action,Mecha,Police,Psychological,Sci-Fi,Seinen,Movie,1,8.34,223036
-4,9989,Ano Hi Mita Hana no Namae wo Bokutachi wa Mada Shiranai.,Drama,Slice of Life,Supernatural,TV,11,8.62,463835
-5,18153,Kyoukai no Kanata,Fantasy,Slice of Life,Supernatural,TV,12,7.88,359011
-6,523,Tonari no Totoro,Adventure,Comedy,Supernatural,Movie,1,8.48,271484
-7,2759,Evangelion: 1.0 You Are (Not) Alone,Action,Mecha,Sci-Fi,Movie,1,8.21,194561
-8,1735,Naruto: Shippuuden,Action,Comedy,Martial Arts,Shounen,Super Power,TV,Unknown,7.94,533578
-9,3784,Evangelion: 2.0 You Can (Not) Advance,Action,Mecha,Sci-Fi,Movie,1,8.53,182224
-10,19815,No Game No Life,Adventure,Comedy,Ecchi,Fantasy,Game,Supernatural,TV,12,8.47,602291
-11,226,Elfen Lied,Action,Drama,Horror,Psychological,Romance,Seinen,Supernatural,TV,13,7.85,623511
-12,10793,Guilty Crown,Action,Drama,Sci-Fi,Super Power,TV,22,7.81,460959
-13,2167,Clannad,Comedy,Drama,Romance,School,Slice of Life,Supernatural,TV,23,8.3,566690
-14,21881,Sword Art Online II,Action,Adventure,Fantasy,Game,Romance,TV,24,7.35,537892
-15,205,Samurai Champloo,Action,Adventure,Comedy,Historical,Samurai,Shounen,TV,26,8.5,390076
-16,10620,Mirai Nikki (TV),Action,Mystery,Psychological,Shounen,Supernatural,Thriller,TV,26,8.07,657190
-17,31043,Boku dake ga Inai Machi,Mystery,Psychological,Seinen,Supernatural,TV,12,8.65,402381
-18,32281,Kimi no Na wa.,Drama,Romance,School,Supernatural,Movie,1,9.37,200630
-19,10087,Fate/Zero,Action,Fantasy,Supernatural,TV,13,8.51,453630
-20,5114,Fullmetal Alchemist: Brotherhood,Action,Adventure,Drama,Fantasy,Magic,Military,Shounen,TV,64,9.26,793665
-21,13601,Psycho-Pass,Action,Police,Psychological,Sci-Fi,TV,22,8.5,509109
-22,1689,Byousoku 5 Centimeter,Drama,Romance,Slice of Life,Movie,3,8.1,324035
-23,1575,Code Geass: Hangyaku no Lelouch,Action,Mecha,Military,School,Sci-Fi,Super Power,TV,25,8.83,715151
-24,12189,Hyouka,Mystery,School,Slice of Life,TV,22,8.17,372246
-25,9756,Mahou Shoujo Madoka★Magica,Drama,Magic,Psychological,Thriller,TV,12,8.51,462974
-26,5,Cowboy Bebop: Tengoku no Tobira,Action,Drama,Mystery,Sci-Fi,Space,Movie,1,8.4,137636
-27,578,Hotaru no Haka,Drama,Historical,Movie,1,8.58,174878
-28,32,Neon Genesis Evangelion: The End of Evangelion,Dementia,Drama,Mecha,Psychological,Sci-Fi,Movie,1,8.45,215630
-29,164,Mononoke Hime,Action,Adventure,Fantasy,Movie,1,8.81,339556
-30,11111,Another,Horror,Mystery,School,Supernatural,Thriller,TV,12,7.88,534657
-31,47,Akira,Action,Adventure,Horror,Military,Sci-Fi,Supernatural,Movie,1,8.15,215897
-32,11757,Sword Art Online,Action,Adventure,Fantasy,Game,Romance,TV,25,7.83,893100
-33,199,Sen to Chihiro no Kamikakushi,Adventure,Drama,Supernatural,Movie,1,8.93,466254
-34,23273,Shigatsu wa Kimi no Uso,Drama,Music,Romance,School,Shounen,TV,22,8.92,416397
-35,21,One Piece,Action,Adventure,Comedy,Drama,Fantasy,Shounen,Super Power,TV,Unknown,8.58,504862
-36,2001,Tengen Toppa Gurren Lagann,Action,Adventure,Comedy,Mecha,Sci-Fi,TV,27,8.78,562962
-37,20,Naruto,Action,Comedy,Martial Arts,Shounen,Super Power,TV,220,7.81,683297
-38,8074,Highschool of the Dead,Action,Ecchi,Horror,Supernatural,TV,12,7.46,535892
-39,513,Tenkuu no Shiro Laputa,Adventure,Fantasy,Romance,Sci-Fi,Movie,1,8.38,151061
-40,5081,Bakemonogatari,Mystery,Romance,Supernatural,Vampire,TV,15,8.39,482268
-41,356,Fate/stay night,Action,Fantasy,Magic,Romance,Supernatural,TV,24,7.58,374880
-42,3588,Soul Eater,Action,Adventure,Comedy,Fantasy,Shounen,Supernatural,TV,51,8.08,580184
-43,6746,Durarara!!,Action,Mystery,Supernatural,TV,24,8.38,556431
-44,28171,Shokugeki no Souma,Ecchi,School,Shounen,TV,24,8.61,348951
-45,16782,Kotonoha no Niwa,Drama,Psychological,Romance,Slice of Life,Movie,1,8.35,189741
-46,28223,Death Parade,Drama,Game,Mystery,Psychological,Thriller,TV,12,8.33,383914
-47,572,Kaze no Tani no Nausicaä,Adventure,Fantasy,Movie,1,8.47,143273
-48,317,Final Fantasy VII: Advent Children,Action,Fantasy,Super Power,Movie,1,7.94,138659
-49,11577,Steins;Gate Movie: Fuka Ryouiki no Déjà vu,Sci-Fi,Thriller,Movie,1,8.61,192424
-50,9919,Ao no Exorcist,Action,Demons,Fantasy,Shounen,Supernatural,TV,25,7.92,583823
-51,12355,Ookami Kodomo no Ame to Yuki,Fantasy,Slice of Life,Movie,1,8.84,226193
-52,14813,Yahari Ore no Seishun Love Comedy wa Machigatteiru.,Comedy,Drama,Romance,School,TV,13,8.12,353876
-53,934,Higurashi no Naku Koro ni,Horror,Mystery,Psychological,Supernatural,Thriller,TV,26,8.17,359494
-54,18897,Nisekoi,Comedy,Harem,Romance,School,Shounen,TV,20,7.91,347599
-55,16498,Shingeki no Kyojin,Action,Drama,Fantasy,Shounen,Super Power,TV,25,8.54,896229
-56,18679,Kill la Kill,Action,Comedy,School,Super Power,TV,24,8.23,508118
-57,9253,Steins;Gate,Sci-Fi,Thriller,TV,24,9.17,673572
-58,7054,Kaichou wa Maid-sama!,Comedy,Romance,School,Shoujo,TV,26,8.26,391628
-59,30276,One Punch Man,Action,Comedy,Parody,Sci-Fi,Seinen,Super Power,Supernatural,TV,12,8.82,552458
-60,11061,Hunter x Hunter (2011),Action,Adventure,Shounen,Super Power,TV,148,9.13,425855
-61,27899,Tokyo Ghoul √A,Action,Drama,Horror,Mystery,Psychological,Seinen,Supernatural,TV,12,7.52,408357
-62,6547,Angel Beats!,Action,Comedy,Drama,School,Supernatural,TV,13,8.39,717796
-63,2251,Baccano!,Action,Comedy,Historical,Mystery,Seinen,Supernatural,TV,13,8.54,363215
-64,14227,Tonari no Kaibutsu-kun,Comedy,Romance,School,Shoujo,Slice of Life,TV,13,7.77,349836
-65,22199,Akame ga Kill!,Action,Adventure,Fantasy,TV,24,7.84,492133
-66,15809,Hataraku Maou-sama!,Comedy,Demons,Fantasy,Romance,Shounen,TV,13,8.03,409037
-67,6880,Deadman Wonderland,Action,Horror,Sci-Fi,TV,12,7.48,453454
-68,2904,Code Geass: Hangyaku no Lelouch R2,Action,Drama,Mecha,Military,Sci-Fi,Super Power,TV,25,8.98,572888
-69,431,Howl no Ugoku Shiro,Adventure,Drama,Fantasy,Romance,Movie,1,8.74,333186
-70,13759,Sakurasou no Pet na Kanojo,Comedy,Drama,Romance,School,Slice of Life,TV,24,8.4,380375
-71,813,Dragon Ball Z,Action,Adventure,Comedy,Fantasy,Martial Arts,Shounen,Super Power,TV,291,8.32,375662
-72,4898,Kuroshitsuji,Action,Comedy,Demons,Fantasy,Historical,Shounen,Supernatural,TV,24,8.06,424919
-73,849,Suzumiya Haruhi no Yuuutsu,Comedy,Mystery,Parody,School,Sci-Fi,Slice of Life,TV,14,8.06,428569
-74,4224,Toradora!,Comedy,Romance,School,Slice of Life,TV,25,8.45,633817
-75,2236,Toki wo Kakeru Shoujo,Adventure,Drama,Romance,Sci-Fi,Movie,1,8.44,289206
-76,121,Fullmetal Alchemist,Action,Adventure,Comedy,Drama,Fantasy,Magic,Military,Shounen,TV,51,8.33,600384
-77,1,Cowboy Bebop,Action,Adventure,Comedy,Drama,Sci-Fi,Space,TV,26,8.82,486824
-78,5681,Summer Wars,Comedy,Sci-Fi,Movie,1,8.3,223359
-79,20507,Noragami,Action,Adventure,Shounen,Supernatural,TV,12,8.17,515378
-80,17265,Log Horizon,Action,Adventure,Fantasy,Game,Magic,Shounen,TV,25,8.14,387100
-81,22319,Tokyo Ghoul,Action,Drama,Horror,Mystery,Psychological,Seinen,Supernatural,TV,12,8.07,618056
-82,512,Majo no Takkyuubin,Adventure,Comedy,Drama,Fantasy,Magic,Romance,Movie,1,8.27,152331
-83,6702,Fairy Tail,Action,Adventure,Comedy,Fantasy,Magic,Shounen,TV,175,8.22,584590
-84,2593,Kara no Kyoukai 1: Fukan Fuukei,Action,Mystery,Supernatural,Thriller,Movie,1,7.81,178880
-85,1535,Death Note,Mystery,Police,Psychological,Supernatural,Thriller,TV,37,8.71,1013917
-86,2025,Darker than Black: Kuro no Keiyakusha,Action,Mystery,Sci-Fi,Super Power,TV,25,8.25,440334
-87,430,Fullmetal Alchemist: The Conqueror of Shamballa,Comedy,Drama,Fantasy,Historical,Military,Shounen,Movie,1,7.74,186465
-88,30,Neon Genesis Evangelion,Action,Dementia,Drama,Mecha,Psychological,Sci-Fi,TV,26,8.32,461946
-89,11617,High School DxD,Comedy,Demons,Ecchi,Harem,Romance,School,TV,12,7.7,398660
-90,7311,Suzumiya Haruhi no Shoushitsu,Comedy,Mystery,Romance,School,Sci-Fi,Supernatural,Movie,1,8.81,240297
-91,31240,Re:Zero kara Hajimeru Isekai Seikatsu,Drama,Fantasy,Psychological,Thriller,TV,25,8.64,355839
-92,2890,Gake no Ue no Ponyo,Adventure,Fantasy,Movie,1,7.93,140277
-93,11981,Mahou Shoujo Madoka★Magica Movie 3: Hangyaku no Monogatari,Drama,Magic,Psychological,Thriller,Movie,1,8.5,135735
-94,269,Bleach,Action,Comedy,Shounen,Super Power,Supernatural,TV,366,7.95,624055
-95,4181,Clannad: After Story,Drama,Fantasy,Romance,Slice of Life,Supernatural,TV,24,9.06,456749
-96,1686,Bleach Movie 1: Memories of Nobody,Action,Adventure,Super Power,Movie,1,7.58,151598
-97,853,Ouran Koukou Host Club,Comedy,Harem,Romance,School,Shoujo,TV,26,8.39,422271
-98,10408,Hotarubi no Mori e,Drama,Romance,Shoujo,Supernatural,Movie,1,8.61,197439
-99,5680,K-On!,Comedy,Music,School,Slice of Life,TV,13,7.87,386048
+
+0"14741"Chuunibyou_demo_Koi_ga_Shitai!"Comedy"Drama"Romance"School"Slice_of_Life"TV"12"7.95,394399
+
+2"22535"Kiseijuu:_Sei_no_Kakuritsu"Action"Drama"Horror"Psychological"Sci-Fi"Seinen"TV"24"8.59"425457
+
+4"9989"Ano_Hi_Mita_Hana_no_Namae_wo_Bokutachi_wa_Mada_Shiranai."Drama"Slice_of_Life"Supernatural"TV"11"8.62,463835
+5"18153"Kyoukai_no_Kanata"Fantasy"Slice_of_Life"Supernatural"TV"12"7.88,359011
+
+
+8"1735"Naruto:_Shippuuden"Action"Comedy"Martial_Arts"Shounen"Super_Power"TV"Unknown"7.94,533578
+
+10"19815"No_Game_No_Life"Adventure"Comedy"Ecchi"Fantasy"Game"Supernatural"TV"12"8.47"602291
+11"226"Elfen_Lied"Action"Drama"Horror"Psychological"Romance"Seinen"Supernatural"TV"13"7.85,623511
+12"10793"Guilty_Crown"Action"Drama"Sci-Fi"Super_Power"TV"22"7.81"460959
+13"2167"Clannad"Comedy"Drama"Romance"School"Slice_of_Life"Supernatural"TV"23"8.3"566690
+14"21881"Sword_Art_Online_II"Action"Adventure"Fantasy"Game"Romance"TV"24"7.35,537892
+15"205"Samurai_Champloo"Action"Adventure"Comedy"Historical"Samurai"Shounen"TV"26"8.5"390076
+16"10620"Mirai_Nikki_(TV)"Action"Mystery"Psychological"Shounen"Supernatural"Thriller"TV"26"8.07"657190
+17"31043"Boku_dake_ga_Inai_Machi"Mystery"Psychological"Seinen"Supernatural"TV"12"8.65"402381
+
+19"10087"Fate/Zero"Action"Fantasy"Supernatural"TV"13"8.51,453630
+20"5114"Fullmetal_Alchemist:_Brotherhood"Action"Adventure"Drama"Fantasy"Magic"Military"Shounen"TV"64"9.26,793665
+21"13601"Psycho-Pass"Action"Police"Psychological"Sci-Fi"TV"22"8.5"509109
+
+23"1575"Code_Geass:_Hangyaku_no_Lelouch"Action"Mecha"Military"School"Sci-Fi"Super_Power"TV"25"8.83"715151
+24"12189"Hyouka"Mystery"School"Slice_of_Life"TV"22"8.17,372246
+25"9756"Mahou_Shoujo_Madoka★Magica"Drama"Magic"Psychological"Thriller"TV"12"8.51"462974
+
+
+
+
+30"11111"Another"Horror"Mystery"School"Supernatural"Thriller"TV"12"7.88,534657
+
+32"11757"Sword_Art_Online"Action"Adventure"Fantasy"Game"Romance"TV"25"7.83,893100
+
+34"23273"Shigatsu_wa_Kimi_no_Uso"Drama"Music"Romance"School"Shounen"TV"22"8.92,416397
+35"21"One_Piece"Action"Adventure"Comedy"Drama"Fantasy"Shounen"Super_Power"TV"Unknown"8.58,504862
+36"2001"Tengen_Toppa_Gurren_Lagann"Action"Adventure"Comedy"Mecha"Sci-Fi"TV"27"8.78,562962
+37"20"Naruto"Action"Comedy"Martial_Arts"Shounen"Super_Power"TV"220"7.81,683297
+38"8074"Highschool_of_the_Dead"Action"Ecchi"Horror"Supernatural"TV"12"7.46"535892
+
+40"5081"Bakemonogatari"Mystery"Romance"Supernatural"Vampire"TV"15"8.39"482268
+41"356"Fate/stay_night"Action"Fantasy"Magic"Romance"Supernatural"TV"24"7.58,374880
+42"3588"Soul_Eater"Action"Adventure"Comedy"Fantasy"Shounen"Supernatural"TV"51"8.08"580184
+43"6746"Durarara!!"Action"Mystery"Supernatural"TV"24"8.38,556431
+44"28171"Shokugeki_no_Souma"Ecchi"School"Shounen"TV"24"8.61,348951
+
+46"28223"Death_Parade"Drama"Game"Mystery"Psychological"Thriller"TV"12"8.33,383914
+
+
+
+50"9919"Ao_no_Exorcist"Action"Demons"Fantasy"Shounen"Supernatural"TV"25"7.92,583823
+
+52"14813"Yahari_Ore_no_Seishun_Love_Comedy_wa_Machigatteiru."Comedy"Drama"Romance"School"TV"13"8.12"353876
+53"934"Higurashi_no_Naku_Koro_ni"Horror"Mystery"Psychological"Supernatural"Thriller"TV"26"8.17,359494
+54"18897"Nisekoi"Comedy"Harem"Romance"School"Shounen"TV"20"7.91,347599
+55"16498"Shingeki_no_Kyojin"Action"Drama"Fantasy"Shounen"Super_Power"TV"25"8.54,896229
+56"18679"Kill_la_Kill"Action"Comedy"School"Super_Power"TV"24"8.23"508118
+57"9253"Steins;Gate"Sci-Fi"Thriller"TV"24"9.17"673572
+58"7054"Kaichou_wa_Maid-sama!"Comedy"Romance"School"Shoujo"TV"26"8.26"391628
+59"30276"One_Punch_Man"Action"Comedy"Parody"Sci-Fi"Seinen"Super_Power"Supernatural"TV"12"8.82,552458
+60"11061"Hunter_x_Hunter_(2011)"Action"Adventure"Shounen"Super_Power"TV"148"9.13"425855
+61"27899"Tokyo_Ghoul_√A"Action"Drama"Horror"Mystery"Psychological"Seinen"Supernatural"TV"12"7.52,408357
+62"6547"Angel_Beats!"Action"Comedy"Drama"School"Supernatural"TV"13"8.39,717796
+63"2251"Baccano!"Action"Comedy"Historical"Mystery"Seinen"Supernatural"TV"13"8.54"363215
+64"14227"Tonari_no_Kaibutsu-kun"Comedy"Romance"School"Shoujo"Slice_of_Life"TV"13"7.77,349836
+65"22199"Akame_ga_Kill!"Action"Adventure"Fantasy"TV"24"7.84,492133
+66"15809"Hataraku_Maou-sama!"Comedy"Demons"Fantasy"Romance"Shounen"TV"13"8.03,409037
+67"6880"Deadman_Wonderland"Action"Horror"Sci-Fi"TV"12"7.48,453454
+68"2904"Code_Geass:_Hangyaku_no_Lelouch_R2"Action"Drama"Mecha"Military"Sci-Fi"Super_Power"TV"25"8.98"572888
+
+70"13759"Sakurasou_no_Pet_na_Kanojo"Comedy"Drama"Romance"School"Slice_of_Life"TV"24"8.4,380375
+71"813"Dragon_Ball_Z"Action"Adventure"Comedy"Fantasy"Martial_Arts"Shounen"Super_Power"TV"291"8.32,375662
+72"4898"Kuroshitsuji"Action"Comedy"Demons"Fantasy"Historical"Shounen"Supernatural"TV"24"8.06,424919
+73"849"Suzumiya_Haruhi_no_Yuuutsu"Comedy"Mystery"Parody"School"Sci-Fi"Slice_of_Life"TV"14"8.06"428569
+74"4224"Toradora!"Comedy"Romance"School"Slice_of_Life"TV"25"8.45"633817
+
+76"121"Fullmetal_Alchemist"Action"Adventure"Comedy"Drama"Fantasy"Magic"Military"Shounen"TV"51"8.33"600384
+77"1"Cowboy_Bebop"Action"Adventure"Comedy"Drama"Sci-Fi"Space"TV"26"8.82"486824
+
+79"20507"Noragami"Action"Adventure"Shounen"Supernatural"TV"12"8.17"515378
+80"17265"Log_Horizon"Action"Adventure"Fantasy"Game"Magic"Shounen"TV"25"8.14"387100
+81"22319"Tokyo_Ghoul"Action"Drama"Horror"Mystery"Psychological"Seinen"Supernatural"TV"12"8.07,618056
+
+83"6702"Fairy_Tail"Action"Adventure"Comedy"Fantasy"Magic"Shounen"TV"175"8.22"584590
+
+85"1535"Death_Note"Mystery"Police"Psychological"Supernatural"Thriller"TV"37"8.71,1013917
+86"2025"Darker_than_Black:_Kuro_no_Keiyakusha"Action"Mystery"Sci-Fi"Super_Power"TV"25"8.25"440334
+
+88"30"Neon_Genesis_Evangelion"Action"Dementia"Drama"Mecha"Psychological"Sci-Fi"TV"26"8.32"461946
+89"11617"High_School_DxD"Comedy"Demons"Ecchi"Harem"Romance"School"TV"12"7.7"398660
+
+91"31240"Re:Zero_kara_Hajimeru_Isekai_Seikatsu"Drama"Fantasy"Psychological"Thriller"TV"25"8.64"355839
+
+
+94"269"Bleach"Action"Comedy"Shounen"Super_Power"Supernatural"TV"366"7.95,624055
+95"4181"Clannad:_After_Story"Drama"Fantasy"Romance"Slice_of_Life"Supernatural"TV"24"9.06,456749
+
+97"853"Ouran_Koukou_Host_Club"Comedy"Harem"Romance"School"Shoujo"TV"26"8.39,422271
+
+99"5680"K-On!"Comedy"Music"School"Slice_of_Life"TV"13"7.87"386048
